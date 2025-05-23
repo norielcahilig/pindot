c81add9b5bf074ff47ef6e3bc3893380dc69a40f
nnn <136660131+norielcahilig@users.noreply.github.com>
2025-05-23 09:06:35 +0800

    Update map.php

--- map.php
+++ map.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("yamabiko.proxy.rlwy.net", "root", "kVmAdxNLIqNrheJTvXcZFBTvWddLnTYp", "railway");
+$conn = new mysqli("yamabiko.proxy.rlwy.net", "root", "kVmAdxNLIqNrheJTvXcZFBTvWddLnTYp", "railway", 3306);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
